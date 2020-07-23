99a722247ea5cdbef2e1152c940d37792c182f90
Reut Levy <reut@hyro.ai>
2020-07-23 14:42:58 +0300

    add 'asyncReduce'

--- src/functional.test.js
+++ src/functional.test.js
@@ -5,12 +5,13 @@ const {
   asyncJuxt,
   asyncMap,
   asyncFilter,
-  keyMap
+  keyMap,
+  asyncReduce,
 } = require("./functional");
 const { multiply, map } = require("ramda");
 
 test("test asyncPipe", async () => {
-  const result = await asyncPipe(asyncIdentity, input =>
+  const result = await asyncPipe(asyncIdentity, (input) =>
     Promise.resolve(multiply(input, 2))
   )(2);
   expect.assertions(1);
@@ -35,11 +36,10 @@ test("test asyncFirst all fail", async () => {
 });
 
 test("test async map", async () => {
-  const result = await asyncMap(input => Promise.resolve(multiply(input, 2)), [
-    1,
-    2,
-    3
-  ]);
+  const result = await asyncMap(
+    (input) => Promise.resolve(multiply(input, 2)),
+    [1, 2, 3]
+  );
 
   expect.assertions(1);
   expect(result).toEqual([2, 4, 6]);
@@ -48,24 +48,24 @@ test("test async map", async () => {
 test("test async juxt", async () => {
   const result = await asyncJuxt([
     asyncIdentity,
-    input => Promise.resolve(map(multiply(2), input))
+    (input) => Promise.resolve(map(multiply(2), input)),
   ])(2, 3);
 
   expect.assertions(1);
   expect(result).toEqual([
     [2, 3],
-    [4, 6]
+    [4, 6],
   ]);
 });
 
 test("test async filter", async () => {
-  const result = await asyncFilter(arg => Promise.resolve(arg % 2 === 0))([
+  const result = await asyncFilter((arg) => Promise.resolve(arg % 2 === 0))([
     1,
     2,
     3,
     4,
     5,
-    6
+    6,
   ]);
 
   expect.assertions(1);
@@ -73,6 +73,17 @@ test("test async filter", async () => {
 });
 
 test("test key map", () => {
-  const result = keyMap(key => key + "2")({ a: 1, b: [1, 2, 3] });
+  const result = keyMap((key) => key + "2")({ a: 1, b: [1, 2, 3] });
   expect(result).toEqual({ a2: 1, b2: [1, 2, 3] });
+});
+
+test("test async reduce", async () => {
+  const result = await asyncReduce(
+    (acc, item) => Promise.resolve(acc + item),
+    0,
+    [1, 2, 3, 4, 5, 6]
+  );
+
+  expect.assertions(1);
+  expect(result).toEqual(21);
 });
