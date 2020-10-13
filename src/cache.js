d9594f862eadd29027da14a15ca8578b9d7819b8
Nitzan Bar <nitzanbar@gmail.com>
2020-10-13 11:40:09 +0300

    fix formatting

--- src/cache.js
+++ src/cache.js
@@ -1,23 +1,19 @@
 import NodeCache from "node-cache";
 import hash from "object-hash";
-import {isNil} from "ramda"
+import { isNil } from "ramda";
 
 export const withCacheAsync = (f, options) => {
   const cache = new NodeCache(options);
-  return withCacheAsyncCustom(
-    cache.get,
-    cache.set,
-    f
-  )
+  return withCacheAsyncCustom(cache.get, cache.set, f);
 };
 
 export const withCacheAsyncCustom = (get, set, f) => async (...args) => {
   const cacheKey = hash.sha1(args);
-  const value = await get(cacheKey)
-  if (!isNil(value)){
-    return value
+  const value = await get(cacheKey);
+  if (!isNil(value)) {
+    return value;
   }
   const result = await f.apply(this, args);
-  set(cacheKey, result)
+  set(cacheKey, result);
   return result;
 };
--- src/cache.test.js
+++ src/cache.test.js
@@ -1,8 +1,8 @@
 const { withCacheAsync } = require("./cache");
 
 test("async cache", async () => {
-  const foo = withCacheAsync(() => Math.random())
-  const random = await foo(5)
-  const cache = await foo(5)
+  const foo = withCacheAsync(() => Math.random());
+  const random = await foo(5);
+  const cache = await foo(5);
   expect(random).toEqual(cache);
 });
