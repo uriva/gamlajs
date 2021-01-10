f66152863f67db22c5d16b81e7a5315b9270fa3e
Erez Haim <erezh.work@gmail.com>
2021-01-10 09:56:01 -0400

    Fix batch to check condition on entire queue

--- src/io.js
+++ src/io.js
@@ -57,7 +57,7 @@ export const batch = (keyFn, maxWaitTime, execute, condition) => {
             pipe(() => delete maxWaitTimeout[key], clearQueue),
             maxWaitTime
           );
-        })(queues[key]);
+        })(map(prop("input"), queues[key]));
       })
   );
 };
