3ace4740c759ff87798b8f33b501608eb2ded20b
david <david@airbud.io>
2022-08-07 19:05:14 +0300

    add throttle to withCacheAsyncCustom

--- src/cache.js
+++ src/cache.js
@@ -1,6 +1,7 @@
 import NodeCache from "node-cache";
 import hash from "object-hash";
 import { isNil } from "ramda";
+import { throttle } from "./lock";
 
 const getCacheKey = (args) => hash.sha1(args);
 
@@ -9,9 +10,8 @@ export const withCacheAsync = (f, options) => {
   return withCacheAsyncCustom(cache.get, cache.set, f);
 };
 
-export const withCacheAsyncCustom =
-  (get, set, f) =>
-  async (...args) => {
+export const withCacheAsyncCustom = (get, set, f) =>
+  throttle(1, async (...args) => {
     const cacheKey = getCacheKey(args);
     const value = await get(cacheKey);
     if (!isNil(value)) {
@@ -20,7 +20,7 @@ export const withCacheAsyncCustom =
     const result = await f.apply(this, args);
     set(cacheKey, result);
     return result;
-  };
+  });
 
 export const deleteCacheAsync =
   (del) =>
