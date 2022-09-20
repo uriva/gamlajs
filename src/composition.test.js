e5ffedadb2f98410c9134ad931fab6415922654a
Uri <uri.valevski@gmail.com>
2022-09-20 08:56:42 +0300

    improve-tests

--- src/composition.test.js
+++ src/composition.test.js
@@ -3,16 +3,20 @@ import { compose, pipe } from "./composition";
 import { multiply } from "./math";
 import { wrapPromise } from "./promise";
 
-test("test pipe", async () => {
-  const result = await pipe(wrapPromise, (input) => Promise.resolve(input * 2))(
-    2
-  );
-  expect.assertions(1);
-  expect(result).toBe(4);
+test("pipe with async functions", async () => {
+  expect(await pipe(wrapPromise, (input) => wrapPromise(input * 2))(2)).toBe(4);
 });
 
-test("test compose", () => {
-  const result = compose((x) => x + 1, multiply(10))(1);
-  expect.assertions(1);
-  expect(result).toBe(11);
+test("compose applies functions in correct order", () => {
+  expect(compose((x) => x + 1, multiply(10))(1)).toBe(11);
+});
+
+test("pipe is able to mix sync and async functions", async () => {
+  expect(
+    await pipe(
+      (x) => x + 1,
+      (x) => wrapPromise(x),
+      (x) => x + 2,
+    )(1),
+  ).toBe(4);
 });
