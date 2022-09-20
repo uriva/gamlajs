7b8c0705406c227ff43bd5c44320ed89201b2b58
Uri <uri.valevski@gmail.com>
2022-09-20 08:15:38 +0300

    operator

---
+++ src/operator.js
@@ -0,0 +1,5 @@
+export const equals = (x) => (y) => x === y;
+export const greater = (x) => (y) => y > x;
+export const smaller = (x) => (y) => y < x;
+export const greaterEquals = (x) => (y) => y >= x;
+export const smallerEquals = (x) => (y) => y <= x;
