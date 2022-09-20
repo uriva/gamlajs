c74701b6aa9844695edc967e2fd2084fb3793c27
Uri <uri.valevski@gmail.com>
2022-09-20 15:52:37 +0300

    reuse

--- src/composition.js
+++ src/composition.js
@@ -1,21 +1,13 @@
-import { empty, reverse, tail } from "./array";
-
-import { isPromise } from "./promise";
-
-const pipeStep =
-  (fs) =>
-  (...x) =>
-    pipe(...tail(fs))(fs[0](...x));
+import { reduce } from "./reduce";
+import { reverse } from "./array";
 
 export const pipe =
   (...fs) =>
   (...x) =>
-    empty(fs)
-      ? x[0]
-      : x.length === 1 && isPromise(x[0])
-      ? x[0].then(pipeStep(fs))
-      : pipeStep(fs)(...x);
-
+    reduce(
+      (s, x) => x(s),
+      () => fs[0](...x),
+    )(fs.slice(1));
 export const compose = (...fs) => pipe(...reverse(fs));
 
 export const after = (f1) => (f2) => pipe(f2, f1);
