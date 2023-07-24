b9c9dc78346e63f4db81209834f43fbdd78361ca
uri <uri.valevski@gmail.com>
2023-07-24 16:40:25 +0300

    deployment

---
+++ build_npm.ts
@@ -0,0 +1,28 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
+
+const outDir = "./dist";
+
+await emptyDir(outDir);
+
+await build({
+  entryPoints: ["./src/index.ts"],
+  outDir,
+  shims: { deno: true },
+  package: {
+    name: "gamla",
+    version: "7.0.0",
+    description: "Functional programming with async",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/uriva/gamlajs.git",
+    },
+    bugs: {
+      url: "https://github.com/uriva/gamlajs/issues",
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync("./LICENSE", outDir + "/LICENSE");
+    Deno.copyFileSync("./README.md", outDir + "/README.md");
+  },
+});
