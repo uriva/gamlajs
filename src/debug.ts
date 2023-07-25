bfc6990e198b72fc4c34569c4144c7cfd7689d05
uri <uri.valevski@gmail.com>
2023-07-25 21:26:29 +0300

    improve typing

--- src/debug.ts
+++ src/debug.ts
@@ -2,8 +2,8 @@ import { sideEffect } from "./composition.ts";
 
 export const log = sideEffect(console.log);
 export const logTable = sideEffect(console.table);
-export const logWith = (...x: unknown[]) =>
-  sideEffect((y) => console.log(...x, y));
+export const logWith = <T>(...x: unknown[]) =>
+  sideEffect<T>((y) => console.log(...x, y));
 
 const getTimestampMilliseconds = () => new Date().getTime();
 
