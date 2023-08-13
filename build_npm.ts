a45772bb9eade7711638a2fdaf9f2102d77c66a9
uri <uri.valevski@gmail.com>
2023-08-13 04:38:05 +0300

    deploy-npm

--- build_npm.ts
+++ build_npm.ts
@@ -10,7 +10,7 @@ await build({
   shims: { deno: true },
   package: {
     name: "gamla",
-    version: "12.0.0",
+    version: "15.0.0",
     description: "Functional programming with async",
     license: "MIT",
     repository: {
