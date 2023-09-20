3894e424779a6486a8d7784e83faad7bad234841
uri <uri.valevski@gmail.com>
2023-09-20 12:36:42 +0300

    better release automation

--- build_npm.ts
+++ build_npm.ts
@@ -10,7 +10,7 @@ await build({
   shims: { deno: true },
   package: {
     name: "gamla",
-    version: "20.0.0",
+    version: Deno.args[0],
     description: "Functional programming with async and type safety",
     license: "MIT",
     repository: {
